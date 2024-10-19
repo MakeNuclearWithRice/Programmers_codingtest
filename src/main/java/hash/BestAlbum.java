c737f0ae6774863ae759e26ee21f386ed274ed97
MakeNuclearWithRice <rlatjdqls34@gmail.com>
2024-10-19 23:48:20 +0900

    Day5 hash

---
+++ src/main/java/hash/BestAlbum.java
@@ -0,0 +1,36 @@
+package hash;
+
+import java.util.*;
+
+public class BestAlbum {
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Integer> count = new HashMap<>();
+        Map<String, PriorityQueue<int[]>> songList = new HashMap<>(); // 장르별 노래를 저장할 우선순위 큐
+
+        for (int i = 0; i < genres.length; i++) {
+            count.put(genres[i], count.getOrDefault(genres[i], 0) + plays[i]);
+
+            songList.putIfAbsent(genres[i], new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1])));
+            songList.get(genres[i]).offer(new int[]{i, plays[i]});
+        }
+
+        List<String> genreOrder = new ArrayList<>(count.keySet());
+        genreOrder.sort((a, b) -> count.get(b) - count.get(a));
+        List<Integer> answerList = new ArrayList<>();
+        for (String genre : genreOrder) {
+            PriorityQueue<int[]> pq = songList.get(genre);
+            int countForGenre = 0;
+            while (!pq.isEmpty() && countForGenre < 2) {
+                answerList.add(pq.poll()[0]);
+                countForGenre++;
+            }
+        }
+
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
